8467bc7c3ee9b69b4774ebedd9c1dcf229f9259f
PRIYAsingh13 <ps597934@gmail.com>
2023-01-10 15:07:13 +0530

    javascript-revision

---
+++ index.js
@@ -0,0 +1,66 @@
+// ===============..==============
+
+// let age = 14;
+
+// let hasVoterCard ='no';
+
+// if(age >= 14 && hasVoterCard =='yes'){
+//     alert("you can vote")
+
+// }else if(age>=14 && hasVoterCard != 'yes'){
+//     alert("get youe voter id card");
+// }else{
+//     alert("you can not vote")
+// }
+
+// ===============. . ============
+
+// if(age >= 14){
+// if(hasVoterCard == 'yes'){
+//     alert("you can vote")
+// }else{
+//     alert("get your voter id card")
+
+// }
+// }else{
+//     alert("you cannot vote")
+// }
+
+// ==============..=============
+
+// let isLoggedin = 5;
+
+// document.write(isLoggedin == 1 ? "logout" : "login");
+// console.log(option)
+// document.write(option)
+
+
+// ================. .================
+// let input = "yess";
+
+// if (input === 1) {
+//     document.write("countinue...");
+
+// } else if (input === "y") {
+//     document.write("continue...");
+
+// } else if (input === "yes") {
+//     document.write("continue...");
+
+// } else if (input === 0) {
+//     document.write("End...")
+
+// } else if (input === "n") {
+//     document.write("end...")
+
+// } else if (input === "no") {
+//     document.write("end...")
+
+// } else if (input === "yess") {
+//     document.write("wrong input")
+// }
+
+//================. .=================
+
+
+
