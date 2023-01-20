73f28572ed27a64b467509db9d417858e6cdd0bb
PRIYAsingh13 <ps597934@gmail.com>
2023-01-20 12:44:33 +0530

    hof

---
+++ Hof.js
@@ -0,0 +1,24 @@
+const hof = (name) =>{
+    if(name === "priya"){
+        return function(topic){
+            console.log(`hii ${name}. What is ${topic}?. plz explain us.`)
+        }
+    }if(name === "singh"){
+        return function(topic){
+            console.log(`hii ${name}. What is ${topic}?. plz explain us.`)
+
+        }
+    }if(name === "Aircampus"){
+        return function(topic){
+            console.log(`hii ${name}. What is ${topic}?. plz explain us.`)
+
+        }
+    }else{
+        return function(){
+            console.log("hof function")
+        }
+    }
+}
+
+const print1 = hof("priya");
+print1("UI")
